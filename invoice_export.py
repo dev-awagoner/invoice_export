6b32ae0a4cbe720df87c335a11981231cf7f9281
dev-awagoner <dev.awagoner@gmail.com>
2019-10-21 23:21:25 +0200

    Adding initial python code

---
+++ invoice_export.py
@@ -0,0 +1,67 @@
+# INVOICE_EXPORT
+# Google Sheets export to MySQL
+
+# ASSUMPTIONS
+# * MySQL Database and Tables are already configured and running on a server
+# * Source sheet names are Invoice, Customer_Info, and Settings
+# * Any IDs, Passwords, etc. will be stored in ENVIRONMENT VARIABLES
+# * Python packages will be previously installed:
+#    * mysql-connector-python
+#    * Sheetfu
+#
+# * Proper credentials and access has been done on the Google side:
+#    * https://console.developers.google.com (creation of Service account)
+#    * Service User email created in above step is given access (via Sharing) to the Sheet 
+#  
+# ENVIRONMENT VARIABLES
+#    SPREADSHEET_ID
+#    SERVICE_ACCESS_FILE
+#    MYSQL_HOST
+#    MYSQL_UID
+#    MYSQL_PWD
+
+import os
+from sheetfu import SpreadsheetApp
+
+# Assign all ENVIRONMENT VARIABLES
+SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
+SERVICE_ACCESS_FILE = os.environ['SERVICE_ACCESS_FILE']
+MYSQL_HOST = os.environ['MYSQL_HOST']
+MYSQL_ID = os.environ['MYSQL_UID']
+MYSQL_PWD = os.environ['MYSQL_PWD']
+
+print(SPREADSHEET_ID)
+print(SERVICE_ACCESS_FILE)
+print(MYSQL_HOST)
+print(MYSQL_ID)
+print(MYSQL_PWD)
+
+spreadsheet = SpreadsheetApp(SERVICE_ACCESS_FILE).open_by_id(SPREADSHEET_ID)
+
+# If necessary to iterate through spreadsheet
+#sheets = spreadsheet.get_sheets()
+#for entries in sheets:print(entries)
+
+# Deliberately hardcoding the sheet names and ranges
+invoice_sheet = spreadsheet.get_sheet_by_name('Invoice')
+invoice_data_range = invoice_sheet.get_range_from_a1(a1_notification='A4:R14')
+
+customer_sheet = spreadsheet.get_sheet_by_name('Customer_Info')
+customer_data_range = customer_sheet.get_range_from_a1('A4:R10')
+
+settings_sheet = spreadsheet.get_sheet_by_name('Settings')
+settings_data_range = settings_sheet.get_range_from_a1('A2:B10')
+
+# Obtain values from ranges
+invoice_values = invoice_data_range.get_values()              # returns a 2D matrix of values
+customer_values = customer_data_range.get_values()
+settings_values = settings_data_range.get_values()
+
+print("\n==========================")
+print(invoice_values)
+
+print("\n==========================")
+print(customer_values)
+
+print("\n==========================\n")
+print(settings_values)
