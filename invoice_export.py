5e98ce699f8b43d5202f4a7e344b7df2adeb8f94
dev-awagoner <dev.awagoner@gmail.com>
2019-10-28 23:15:47 +0100

    Added Invoice code and DATE updates for *_values

--- invoice_export.py
+++ invoice_export.py
@@ -6,12 +6,12 @@
 # * Source sheet names are Invoice, Customer_Info, and Settings
 # * Any IDs, Passwords, etc. will be stored in ENVIRONMENT VARIABLES
 # * Python packages will be previously installed:
-#    * mysql-connector-python
-#    * Sheetfu
+#    - mysql-connector-python
+#    - Sheetfu
 #
 # * Proper credentials and access has been done on the Google side:
-#    * https://console.developers.google.com (creation of Service account)
-#    * Service User email created in above step is given access (via Sharing) to the Sheet 
+#    - https://console.developers.google.com (creation of Service account)
+#    - Service User email created in above step is given access (via Sharing) to the Sheet 
 #  
 # ENVIRONMENT VARIABLES
 #    SPREADSHEET_ID
@@ -20,6 +20,7 @@
 #    MYSQL_UID
 #    MYSQL_PWD
 
+import datetime
 import os
 import sys
 from sheetfu import SpreadsheetApp
@@ -37,7 +38,7 @@ if missing:
     print("\nEnvironment variables that do not exist: %s" % missing)
     sys.exit()
 else:
-    print("OK.")
+    print("COMPLETE")
     try: 
         SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
         SERVICE_ACCESS_FILE = os.environ['SERVICE_ACCESS_FILE']
@@ -61,49 +62,90 @@ spreadsheet = SpreadsheetApp(SERVICE_ACCESS_FILE).open_by_id(SPREADSHEET_ID)
 #sheets = spreadsheet.get_sheets()
 #for entries in sheets:print(entries)
 
-# Deliberately hardcoding the sheet names and ranges
+# Deliberately hardcoding the Invoice sheet name and ranges (NEED TO DETERMINE RANGE)
+# Invoice Sheet
 invoice_sheet = spreadsheet.get_sheet_by_name('Invoice')
-invoice_data_range = invoice_sheet.get_range_from_a1('A4:R14')
+invoice_data_range = invoice_sheet.get_range_from_a1('A4:Q14')
+invoice_values = invoice_data_range.get_values()    # returns a 2D matrix of values
 
+# Change INVOICE DATEs format (DD/MM?YYY -> YYYY/MM/DD) to import into MySQL date column
+for row in invoice_values:
+    row[1] = datetime.datetime.strptime(row[1], '%d/%m/%Y').strftime('%Y/%m/%d')
+
+# Deliberately hardcoding the Customer sheet name and ranges (NEED TO DETERMINE RANGE)
+# Customer_Info Sheet
 customer_sheet = spreadsheet.get_sheet_by_name('Customer_Info')
 customer_data_range = customer_sheet.get_range_from_a1('A4:R10')
+customer_values = customer_data_range.get_values()
 
+# Change CUSTOMER DATEs format (DD/MM?YYY -> YYYY/MM/DD) to import into MySQL date column
+for row in customer_values:
+    row[1] = datetime.datetime.strptime(row[1], '%d/%m/%Y').strftime('%Y/%m/%d')
+    row[2] = datetime.datetime.strptime(row[2], '%d/%m/%Y').strftime('%Y/%m/%d')
+
+# Settings Sheet
 settings_sheet = spreadsheet.get_sheet_by_name('Settings')
 settings_data_range = settings_sheet.get_range_from_a1('A2:B9')
-
-# Obtain values from ranges
-invoice_values = invoice_data_range.get_values()        # returns a 2D matrix of values
-customer_values = customer_data_range.get_values()
 settings_values = settings_data_range.get_values()
 
-print("\n==========================")
-print(invoice_values)
-
-print("\n==========================")
-print(customer_values)
-
-print("\n==========================\n")
-print(settings_values)
-
+# DEBUG Values
+#print("\n==========================")
+#print(invoice_values)
+#print("\n==========================")
+#print(customer_values)
+#print("\n==========================\n")
+#print(settings_values)
 
 # MySQL Connection
 try:
     connection = mysql.connector.connect(host=MYSQL_HOST, database=MYSQL_DATABASE, user=MYSQL_ID, password=MYSQL_PWD)
 
     if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version: ", db_Info)
+        # Keep this for DEBUG
+        # db_Info = connection.get_server_info()
+        # print("Connected to MySQL Server version: ", db_Info)
+        # cursor = connection.cursor()
+        # cursor.execute("select database();")
+        # record = cursor.fetchone()
+        # print("You are connected to database: ", record)
+        
+        # Insert SETTINGS data
+        print("Inserting Settings data...", end=".")
+        Item_Insert_Query = "INSERT INTO settings(description, id) VALUES(%s,%s)"
         cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You are connected to database: ", record)
 
-        mySql_Item_Insert_Query = "INSERT INTO Item(description, id) VALUES(%s,%s)"
-        cursor = connection.cursor()
-        result = cursor.executemany(mySql_Item_Insert_Query, settings_values)
+        # Technically don't need to use result unless it's necessary to check
+        # the results of cursor.executemany()
+        result = cursor.executemany(Item_Insert_Query, settings_values)
         connection.commit()
-        print("Record inserted successfully into Item table")
         cursor.close()
+        print("COMPLETE")
+
+        # Insert CUSTOMER data
+        print("Inserting Customer data...", end=".")
+        Customer_Insert_Query = "INSERT INTO customer(cycle, start_date, end_date, item, price, " \
+                                "iva, total, customer_id, business_name, business_id, contact, address, " \
+                                "city, province, postcode, email, phone, phone2) " \
+                                "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor = connection.cursor()
+        result = cursor.executemany(Customer_Insert_Query, customer_values)
+        #print(result)
+        connection.commit()
+        cursor.close()
+        print("COMPLETE")
+
+        # Insert INVOICE data
+        print("Inserting Invoice data...", end=".")
+        Invoice_Insert_Query = "INSERT INTO invoice(invoice_number, date, item, price, iva, total, " \
+                               "customer_id, business_name, business_id, contact, address, city, " \
+                               "province, postcode, email, phone, phone2) " \
+                               "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor = connection.cursor()
+        result = cursor.executemany(Invoice_Insert_Query, invoice_values)
+        #print(result)
+        connection.commit()
+        cursor.close()
+        print("COMPLETE")
 
 except Error as e:
     print("Error while connecting to MySQL", e)
@@ -112,20 +154,3 @@ finally:
         cursor.close()
         connection.close()
         print("MySQL connection is closed")
-
-
-'''
-def main():
-   # books = [('Harry Potter And The Order Of The Phoenix', '9780439358071'),
-   #      ('Gone with the Wind', '9780446675536'),
-   #          ('Pride and Prejudice (Modern Library Classics)', '9780679783268')]
-    insert_item(settings_values)
-
-if __name__ == '__main__':
-    main()
-
-def insert_item(description, id):
-    query = "INSERT INTO books(description, id) " \
-            "VALUES(%s,%s)"
-    args = (description, id)
-'''
