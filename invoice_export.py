1dd4d0216211caa186880497088b6e19e6482866
dev-awagoner <dev.awagoner@gmail.com>
2019-11-05 15:55:07 +0100

    Using Context Manager for MySQL connection

--- invoice_export.py
+++ invoice_export.py
@@ -28,50 +28,78 @@ from sheetfu import SpreadsheetApp
 import mysql.connector
 from mysql.connector import Error
 
-def num2col(number):
-    # Converts number to  Excel-style column name, e.g. 1 = A, 26 = Z, 27 = AA
-    name = ''
-    while number > 0:
-        number, r = divmod (number - 1, 26)
-        name = chr(r + ord('A')) + name
-    return name
+class UseDatabase:
+    def __init__(self, config: dict):
+        self.configuration = config
 
-env_vars = ['SPREADSHEET_ID', 'SERVICE_ACCESS_FILE', 'MYSQL_HOST', 'MYSQL_DATABASE',
-            'MYSQL_UID', 'MYSQL_PWD']
+    def __enter__(self) -> 'cursor':
+        # Connect to database and create a DB cursor.
+        # Return the database cursor to the context manager.
+        
+        try:
+            print("Connecting to MySQL..", end=".")
+            self.connection = mysql.connector.connect(**self.configuration)
+            
+            if self.connection.is_connected():
+                self.connection = mysql.connector.connect(**self.configuration)
+                self.cursor = self.connection.cursor()
+                print("CONNECTED")
+                return self.cursor
 
-# Check and Assign ENVIRONMENT VARIABLES
-print("Checking if all environment variables are set..", end='.')
+        except Error as e:
+            print("Error while connecting to MySQL", e)
 
-missing = set(env_vars) - set(os.environ)
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """Destroy the cursor as well as the connection (after committing).
+        """
+        self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
+        print("MySQL connection...CLOSED")
+
+# Setting the expected ENVIRONMENT VARIABLES
+env_vars = {
+    'sheet_id': 'SPREADSHEET_ID',
+    'service_access_file': 'SERVICE_ACCESS_FILE',
+    'host': 'MYSQL_HOST',
+    'database': 'MYSQL_DATABASE',
+    'user': 'MYSQL_UID',
+    'password': 'MYSQL_PWD'
+    #,'test': 'test'    # To test MISSING Environment Variable
+}
+
+# Checking existence of ENVIRONMENT VARIABLES and building CONFIG dictionaries
+print("Checking environment variables..", end='.')
+env_list = [ v for v in env_vars.values() ]
+
+missing = set(env_list) - set(os.environ)
 if missing:
-    print("\nEnvironment variables that do not exist: %s" % missing)
+    print("\nEnvironment variables that are not set: %s" % missing)
     sys.exit()
 else:
-    try: 
-        SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
-        SERVICE_ACCESS_FILE = os.environ['SERVICE_ACCESS_FILE']
-        MYSQL_HOST = os.environ['MYSQL_HOST']
-        MYSQL_DATABASE = os.environ['MYSQL_DATABASE']
-        MYSQL_ID = os.environ['MYSQL_UID']
-        MYSQL_PWD = os.environ['MYSQL_PWD']
-        print("COMPLETE")
-
+    try:
+        env_config = {k: os.environ.get(v) for k, v in env_vars.items() if v in os.environ}
+        
     except KeyError:  
-        print("A problem with one of the environment variables exists.")
-        print("Exiting")
+        print("A problem with one or more of the environment variables exists...EXITING")
         sys.exit()
 
-# Add try block to handle case where SERVICE_ACCESS_FILE or SPREADSHEET_ID
-# do not exist, or incorrect.  
+# Create a dictionary subset of ENV_CONFIG for just the MySQL parameters
+db_config = {k: env_config[k] for k in env_config.keys() & {'host', 'database', 'user', 'password'}}
+print("COMPLETE")
+
+# Add try block to handle case where SERVICE_ACCESS_FILE or SPREADSHEET_ID do not exist, or are  incorrect?  
 # Add a fileExists check for SERVICE_ACCESS_FILE ?
 # Add HttpError if spreadsheet does not exist ?
-spreadsheet = SpreadsheetApp(SERVICE_ACCESS_FILE).open_by_id(SPREADSHEET_ID)
 
+print("Connecting to Spreadsheet..", end='.')
+spreadsheet = SpreadsheetApp(env_config.get('service_access_file')).open_by_id(env_config.get('sheet_id'))
+print("CONNECTED")
 # If necessary to iterate through spreadsheet
 #sheets = spreadsheet.get_sheets()
 #for entries in sheets:print(entries)
 
-# Deliberately hardcoding the Invoice sheet name and ranges (NEED TO DETERMINE RANGE)
+# Deliberately hardcoding the Invoice sheet name
 # Invoice Sheet
 invoice_sheet = spreadsheet.get_sheet_by_name('Invoice')
 invoice_sheet_range = invoice_sheet.get_data_range()
@@ -84,7 +112,7 @@ invoice_values.pop(0); invoice_values.pop(0); invoice_values.pop(0)
 for row in invoice_values:
     row[1] = datetime.datetime.strptime(row[1], '%d/%m/%Y').strftime('%Y/%m/%d')
 
-# Deliberately hardcoding the Customer sheet name and ranges (NEED TO DETERMINE RANGE)
+# Deliberately hardcoding the Customer sheet name 
 # Customer_Info Sheet
 customer_sheet = spreadsheet.get_sheet_by_name('Customer_Info')
 customer_sheet_range = customer_sheet.get_data_range()
@@ -98,6 +126,7 @@ for row in customer_values:
     row[1] = datetime.datetime.strptime(row[1], '%d/%m/%Y').strftime('%Y/%m/%d')
     row[2] = datetime.datetime.strptime(row[2], '%d/%m/%Y').strftime('%Y/%m/%d')
 
+# Deliberately hardcoding the Settings sheet name 
 # Settings Sheet
 settings_sheet = spreadsheet.get_sheet_by_name('Settings')
 settings_sheet_range = settings_sheet.get_data_range()
@@ -114,61 +143,31 @@ settings_values.pop(0)
 #print("\n==========================\n")
 #print(settings_values)
 
-# MySQL Connection
-try:
-    connection = mysql.connector.connect(host=MYSQL_HOST, database=MYSQL_DATABASE, user=MYSQL_ID, password=MYSQL_PWD)
 
-    if connection.is_connected():
-        # Keep this for DEBUG
-        # db_Info = connection.get_server_info()
-        # print("Connected to MySQL Server version: ", db_Info)
-        # cursor = connection.cursor()
-        # cursor.execute("select database();")
-        # record = cursor.fetchone()
-        # print("You are connected to database: ", record)
-        
-        # Insert SETTINGS data
-        print("Inserting Settings data..", end=".")
-        Item_Insert_Query = "INSERT INTO settings(description, id) VALUES(%s,%s)"
-        cursor = connection.cursor()
+with UseDatabase(db_config) as cursor:
+    # Insert SETTINGS data
+    print("Inserting Settings data..", end=".")
+    Item_Insert_Query = "INSERT INTO settings(description, id) VALUES(%s,%s)"
 
-        # Technically don't need to use result unless it's necessary to check
-        # the results of cursor.executemany()
-        result = cursor.executemany(Item_Insert_Query, settings_values)
-        connection.commit()
-        cursor.close()
-        print("COMPLETE")
+    # Technically don't need to use result unless it's necessary to check
+    # the results of cursor.executemany()
+    result = cursor.executemany(Item_Insert_Query, settings_values)
+    print("COMPLETE")
 
-        # Insert CUSTOMER data
-        print("Inserting Customer data..", end=".")
-        Customer_Insert_Query = "INSERT INTO customer(cycle, start_date, end_date, item, price, " \
+    # Insert CUSTOMER data
+    print("Inserting Customer data..", end=".")
+    Customer_Insert_Query = "INSERT INTO customer(cycle, start_date, end_date, item, price, " \
                                 "iva, total, customer_id, business_name, business_id, contact, address, " \
                                 "city, province, postcode, email, phone, phone2) " \
                                 "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        cursor = connection.cursor()
-        result = cursor.executemany(Customer_Insert_Query, customer_values)
-        #print(result)
-        connection.commit()
-        cursor.close()
-        print("COMPLETE")
+    result = cursor.executemany(Customer_Insert_Query, customer_values)
+    print("COMPLETE")
 
-        # Insert INVOICE data
-        print("Inserting Invoice data...", end=".")
-        Invoice_Insert_Query = "INSERT INTO invoice(invoice_number, date, item, price, iva, total, " \
+    # Insert INVOICE data
+    print("Inserting Invoice data...", end=".")
+    Invoice_Insert_Query = "INSERT INTO invoice(invoice_number, date, item, price, iva, total, " \
                                "customer_id, business_name, business_id, contact, address, city, " \
                                "province, postcode, email, phone, phone2) " \
                                "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        cursor = connection.cursor()
-        result = cursor.executemany(Invoice_Insert_Query, invoice_values)
-        #print(result)
-        connection.commit()
-        cursor.close()
-        print("COMPLETE")
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if (connection.is_connected()):
-        cursor.close()
-        connection.close()
-        #print("MySQL connection is closed")
+    result = cursor.executemany(Invoice_Insert_Query, invoice_values)
+    print("COMPLETE")
